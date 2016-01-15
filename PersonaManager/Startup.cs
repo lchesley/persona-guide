8351e99a3eedb964c5c8665ce2b74066780a3e1b
lchesley <lisachesley@gmail.com>
2016-01-14 22:58:28 -0500

    New project
    new projects in keeping with analysis

---
+++ PersonaManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonaManager.Startup))]
+namespace PersonaManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
