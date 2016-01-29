de61315339b36e865f3ac145f2fb98b0313dc4a1
lchesley <lisachesley@gmail.com>
2016-01-29 11:59:58 -0500

    Further Model Changes
    Added final classes to Persona.

--- PersonaManager/Entities/Persona.cs
+++ PersonaManager/Entities/Persona.cs
@@ -12,8 +12,34 @@ namespace PersonaManager.Entities
         public int InitialLevel { get; set; }
         public int HPIncrease { get; set; }
         public int SPIncrease { get; set; }
-        public string SkillsList { get; set; }
+        public bool IsDownloadedContent { get; set; }
         public List<LearnedSkill> LearnedSkills { get; set; }
-        public Skill ExtractedSkill { get; set; }     
+        public Skill ExtractedSkill { get; set; }
+        public PersonaInheritanceType InheritanceType { get; set; }
+        public List<SkillInheritance> InheritableSkillTypes { get; set; }
+        public string SkillsList
+        {
+            get
+            {
+                string temp = String.Empty;
+
+                if (LearnedSkills != null && LearnedSkills.Count > 0)
+                {
+                    foreach (LearnedSkill skill in LearnedSkills)
+                    {
+                        if (skill.LevelLearned == 1)
+                        {
+                            temp += String.Format("{0}, ", skill.Skill.Name);
+                        }
+                        else
+                        {
+                            temp += String.Format("{0}({1}), ", skill.Skill.Name, skill.LevelLearned);
+                        }
+                    }
+                }
+
+                return temp.Remove(temp.Length - 2);
+            }
+        }
     }
 }
---
+++ PersonaManager/Entities/SkillInheritance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonaManager.Entities
+{
+    public class SkillInheritance
+    {
+        public SkillInheritanceType Type { get; set; }
+        public bool CanInherit { get; set; }
+    }
+}
